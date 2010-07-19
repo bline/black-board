f2670a6d904993e40afeb286416f0be266948d11
Scott Beck <scottbeck@gmail.com>
2010-07-18 22:34:30 -0400

    * is now a full class * added abstract * added "with MooseX::Param", taken from Black::Board::Message::Simple,   which is no longer used. * added use MooseX::Clone for cloning * updated documentation, added See Also section

--- lib/Black/Board/Message.pm
+++ lib/Black/Board/Message.pm
@@ -1,8 +1,14 @@
+use strict;
+use warnings;
 use MooseX::Declare;
 
-role Black::Board::Message
+#ABSTRACT: param based Message for L<Bread::Board>
+
+class Black::Board::Message
+    with MooseX::Param
     with Black::Board::Trait::Traversable
 {
+    use MooseX::Clone;
 =attr C<bubble>
 
 L<Black::Board::Subscriber> uses this flag to know if it should continue
@@ -22,17 +28,38 @@ dispatching the current subscription message.
 
 This makes sense from the context of a L<Black::Board::Subscriber> subscription
 callback. It allows you to cancel the current chain of subscriber dispatch.
-This is usually done in end-point subscribers. An example of an end-point is
-the subscriber in a LogDispatch subscription chain that dispatches to the log
-object. 
+This is usually done in end-point* subscribers. This object is cloned and bubble
+set to false in the clone.
+
+* An example of an end-point is the subscriber in a LogDispatch subscription
+chain that dispatches to the log object. 
 
 =cut
 
     method cancel_bubble {
-        $self->bubble(0);
-        return $self;
+        return $self->clone( bubble => 0 );
+    }
+
+=method C<clone_with_params>
+
+=cut
+
+    method clone_with_params( HashRef $params ) {
+        return $self->clone(
+            params => $self->params->merge( $params )
+        );
     }
 }
+
+=head1 SEE ALSO
+
+=for :list
+* L<Black::Board::Publisher> - publishes Messages to L<Topics|Black::Board::Topic>
+* L<Black::Board::Topic> - delivers Messages to L<Subscribers|Black::Board::Subscriber>
+* L<Black::Board::Subscriber> - receives a Message
+* L<Black::Board> - provides sugar syntax to do all this
+
+=cut
 
 1;
 
