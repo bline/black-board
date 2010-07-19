654f0af018468baec67e2d626d6f90f1014a7399
Scott Beck <scottbeck@gmail.com>
2010-07-18 22:38:53 -0400

    * General     * added explicit strictures for Critic tests     * added missing "use Scalar::Util"
    * Docs
        * added abstract
        * add DESCRIPTION
        * added See Also section
    
    * Topic object
        * publish now takes a Topic Object instead of a TopicNameList
        * added get_topic
        * import Topic, TopicName from Black::Board::Types
        * remove unused find_topic
        * added first_topic
        * added remove_topic

--- lib/Black/Board/Publisher.pm
+++ lib/Black/Board/Publisher.pm
@@ -1,14 +1,29 @@
+use strict;
+use warnings;
 use MooseX::Declare;
+
+# ABSTRACT: Publisher object for L<Black::Board>, dispatches to Topic subscribers
 
 class Black::Board::Publisher
     with Black::Board::Trait::Traversable
 {
+
+    use Scalar::Util qw( blessed reftype );
     use Black::Board::Types qw(
         TopicNameList
         TopicList
+        Topic
+        TopicName
         Message
     );
     use Moose::Autobox;
+
+=head1 DESCRIPTION
+
+This is the Publisher object for L<Black::Board>. It contains a list of
+L<Topic|Black::Board::Topic> objects. The C<Publisher> object takes care of
+this list of C<Topic> objects, it handles dispatching to them and provides
+methods to add and remove topics.
 
 =attr C<topics>
 
@@ -28,9 +43,9 @@ Registers the specified L<Black::Board::Topic> object to this publisher.
 
 Returns a list of L<Black::Board::Topic> objects registered to this publisher.
 
-=method C<find_topic>
+=method C<first_topic>
 
-Peform a grep operation on L<topics|/ATTRIBUTES/topics>.
+Peform a L<first()|List::Util> operation on L<topics|/ATTRIBUTES/topics>.
 
 =cut
 
@@ -45,66 +60,71 @@ Peform a grep operation on L<topics|/ATTRIBUTES/topics>.
             $_->parent( $self ) for @{ $self->topics };
         },
         handles => {
-            has_topics => 'count',
-            add_topic  => 'push',
-            topic_list => 'elements',
-            find_topic => 'grep',
+            has_topics  => 'count',
+            add_topic   => 'push',
+            topic_list  => 'elements',
+            first_topic => 'first',
         }
     );
 
-=method C<find_topics>
+    before add_topic( Topic $topic ) {
+        if ( $self->first_topic( sub { $_->name eq $topic->name } ) ) {
+            confess "'" . $topic->name . "' has already been registered";
+        }
+    }
 
-Finds and returns an Array of Topic objects given the TopicNameList specified.
+=method C<remove_topic>
+
+Only argument is a L<Black::Board::Types/TYPES/TopicName>. Removes the given
+TopicName from this publishers list of topics.
 
 =cut
 
-    method find_topics( TopicNameList $topics ) {
-        $self->find_topic(
-            sub {
-                my $topic = $_;
-                $topics->grep(
-                    sub {
-                        my $topic_name = $_;
-                        $topic->name eq $topic_name
-                    }
-                );
+    method remove_topic( TopicName $topic_name ) {
+        my $topics = $self->topics;
+        for ( my $i = 0; $i < $topics->count; ++$i ) {
+            if ( $topics->get( $i )->name eq $topic_name ) {
+                $topics->delete( $i );
+                last;
             }
-        );
+        }
+    }
+
+=method C<get_topic>
+
+Given a TopicName, returns a Topic object if found, undefined otherwise.
+
+=cut
+
+    method get_topic( TopicName $topic_name ) {
+        return $self->first_topic( sub { $_->name eq $topic_name } );
     }
 
 =method C<publish>
 
-Takes a L<Black::Board::Types/TYPES/TopicNameList> and a
+Takes a L<Black::Board::Types/TYPES/Topic> and a
 L<Black::Board::Types/TYPES/Message>. The message is dispatched to the
-subscribers of the topics. The final message returned by the subscriber is
+subscribers of the topic. The final message returned by the subscriber is
 returned.
 
 =cut
 
-    method publish( TopicNameList :$topics, Message :$message ) {
+    method publish( Topic :$topic, Message :$message ) {
 
-        # get the cross section of topics whos names match the array $topic_names
-        $topics = $self->find_topics( $topics );
+        for my $subscriber ( $topic->subscriber_list->reverse->flatten ) {
 
-        # topics from last specified to first specified. this order was chosen
-        # because it follows the order in which subscribers are dispatched. deeper
-        # in the Black::Board tree can override the topics higher up
-        for my $topic ( $topics->reverse->flatten ) {
-            for my $subscriber ( $topic->subscriber_list->reverse->flatten ) {
+            # if the subscriber wishes to change the message, they must
+            # clone it. the return copy is what bubbles up. deliver() must
+            # return the original message or a clone of it.
+            $message = $topic->deliver(
+                subscriber => $subscriber,
+                publisher  => $self,
+                message    => $message
+            );
 
-                # if the subscriber wishes to change the message, they must
-                # clone it. the return copy is what bubbles up. deliver() must
-                # return the original message or a clone of it.
-                $message = $topic->deliver(
-                    subscriber => $subscriber,
-                    publisher  => $self,
-                    message    => $message
-                );
-
-                # this boolean is set to false when cancel_bubble() is called.
-                # cancel_bubble() is used by final-destination subscribers
-                last unless $message->bubble;
-            }
+            # this boolean is set to false when cancel_bubble() is called.
+            # cancel_bubble() is used by final-destination subscribers
+            last unless $message->bubble;
         }
 
         # the final message returned is expected to have information about what
@@ -112,5 +132,14 @@ returned.
         return $message;
     }
 }
+
+=head1 SEE ALSO
+
+=for :list
+* L<Black::Board::Message> - objects being dispatched to L<Topics|Black::Board::Topic>.
+* L<Black::Board::Topic> - topic objects contained and dispatched to by a Publisher
+* L<Black::Board> - provides sugar syntax to a singleton Publisher
+
+=cut
 
 1;
